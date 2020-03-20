87beb410a4797bc95fc79c9c61f6cf9f7e26b991
Icarus-Alpha1264 <17162810+Icarus-Alpha1264@users.noreply.github.com>
2020-03-20 18:02:10 +0800

    新增测试代码

--- test_example.py
+++ test_example.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # Test for age_anime_homepage_collector.py
-from age_anime_homepage_collector import AnimeCollector
+# from age_anime_homepage_collector import AnimeCollector
+# anime_collector = AnimeCollector()
+# anime_collector.run()
+
+# Test for age_anime_recommended_daily_collector.py
+from age_anime_recommended_daily_collector import AnimeCollector
 anime_collector = AnimeCollector()
 anime_collector.run()
